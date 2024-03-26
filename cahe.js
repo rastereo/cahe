9aa0bc03ad5129b823953b2d076d4e2b5512520c
Kaptur <Rostislav.Kaptur@gefera.ru>
2024-03-26 15:21:59 +0300

    feat: create Cahe class

--- cahe.js
+++ cahe.js
@@ -1,93 +1,107 @@
 #!/usr/bin/env node
 
+// eslint-disable-next-line import/no-unresolved
 import { crush } from "html-crush"; // https://codsen.com/os/html-crush
+// eslint-disable-next-line no-unused-vars
 import archiver from "archiver"; // https://www.archiverjs.com/
 import fs from "fs";
 import path from "path";
 
-const htmlFilePath = process.argv[2];
+class Cahe {
+  constructor(htmlFilePath) {
+    this.FilePath = htmlFilePath;
+    this.htmlCrushConfig = {
+      lineLengthLimit: 500,
+      removeIndentations: true,
+      removeLineBreaks: true,
+      removeHTMLComments: true,
+      removeCSSComments: true,
+      reportProgressFunc: null,
+      reportProgressFuncFrom: 0,
+      reportProgressFuncTo: 100,
+    };
+  }
 
-const htmlCrushConfig = {
-  lineLengthLimit: 500,
-  removeIndentations: true,
-  removeLineBreaks: true,
-  removeHTMLComments: true,
-  removeCSSComments: true,
-  reportProgressFunc: null,
-  reportProgressFuncFrom: 0,
-  reportProgressFuncTo: 100,
-};
-
-const dirPath = getDirPath(htmlFilePath);
-const fileName = getFileName(htmlFilePath);
-const newFileName = createNewFileName(fileName);
-
-function getDirPath(htmlPath) {
-  // eslint-disable-next-line no-plusplus
-  for (let i = htmlPath.length - 1; i > 0; i--) {
-    if (htmlPath[i] === "/" || htmlPath[i] === "\\") {
-      return htmlPath.slice(0, i + 1);
+  // eslint-disable-next-line consistent-return
+  #getDirPath() {
+    // eslint-disable-next-line no-plusplus
+    for (let i = this.FilePath.length - 1; i > 0; i--) {
+      if (this.FilePath[i] === "/" || this.FilePath[i] === "\\") {
+        return this.FilePath.slice(0, i + 1);
+      }
     }
   }
-}
 
-function getFileName(htmlPath) {
-  // eslint-disable-next-line no-plusplus
-  for (let i = htmlPath.length - 1; i > 0; i--) {
-    if (htmlPath[i] === "/" || htmlPath[i] === "\\") {
-      return htmlPath.slice(i + 1);
+  // eslint-disable-next-line consistent-return
+  #getFileName() {
+    // eslint-disable-next-line no-plusplus
+    for (let i = this.FilePath.length - 1; i > 0; i--) {
+      if (this.FilePath[i] === "/" || this.FilePath[i] === "\\") {
+        return this.FilePath.slice(i + 1);
+      }
     }
   }
-}
 
-function createNewFileName(fileName) {
-  return `${fileName.slice(0, -4)}min.html`;
-}
+  #createNewFileName() {
+    return `${this.fileName.slice(0, -4)}min.html`;
+  }
 
-async function importHtmlAndConvertToString(filePath) {
-  try {
-    if (!filePath) {
+  // eslint-disable-next-line consistent-return
+  async #importHtmlAndConvertToString() {
+    try {
+      const data = await fs.promises.readFile(path.resolve(this.FilePath), {
+        encoding: "utf-8",
+      });
+
+      console.log("\x1b[32m", "Converted to a string successfully");
+
+      return data;
+    } catch (error) {
+      console.error("\x1b[31m", "Error reading the HTML file", error);
+    }
+  }
+
+  // eslint-disable-next-line consistent-return
+  async #minifyHtml(htmlString) {
+    try {
+      const { log, result } = crush(htmlString, this.htmlCrushConfig);
+
+      console.log("\x1b[32m", "Html-crush minify successfully");
+      console.log("\x1b[0m", log);
+
+      return result;
+    } catch (error) {
+      console.error("\x1b[31m", "HTML-Crush server error", error);
+    }
+  }
+
+  async writeNewHtmlFile() {
+    if (!this.FilePath) {
       console.error("\x1b[31m", "Please provide a file path.");
 
       return;
     }
-    const data = await fs.promises.readFile(path.resolve(filePath), { encoding: "utf-8" });
 
-    console.log("\x1b[32m", "Converted to a string successfully");
+    try {
+      this.dirPath = this.#getDirPath();
+      this.fileName = this.#getFileName();
+      this.newFileName = this.#createNewFileName();
 
-    return data;
-  } catch (error) {
-    console.error("\x1b[31m", "Error reading the HTML file", error);
+      const outputPath = path.join(this.dirPath, this.newFileName);
+
+      const resultHtmlString = await this.#importHtmlAndConvertToString();
+
+      const minifiedHtml = await this.#minifyHtml(resultHtmlString);
+
+      await fs.promises.writeFile(outputPath, minifiedHtml, "utf-8");
+
+      console.log("\x1b[32m", `File has been write to ${this.dirPath}`);
+    } catch (error) {
+      console.error("\x1b[31m", "Error writing the file:", error);
+    }
   }
 }
 
-async function minifyHtml(filePath) {
-  try {
-    const htmlString = await importHtmlAndConvertToString(filePath);
+const cahe = new Cahe(process.argv[2]);
 
-    const { log, result } = crush(htmlString, htmlCrushConfig);
-
-    console.log("\x1b[32m", "Html-crush minify successfully");
-    console.log("\x1b[0m", log);
-
-    return result;
-  } catch (error) {
-    console.error("\x1b[31m", "HTML-Crush server error", error);
-  }
-}
-
-async function writeNewHtmlFile() {
-  try {
-    const outputPath = path.join(dirPath, newFileName);
-
-    const minifiedHtml = await minifyHtml(htmlFilePath);
-
-    await fs.promises.writeFile(outputPath, minifiedHtml, "utf-8");
-
-    console.log("\x1b[32m", `File has been write to ${dirPath}`);
-  } catch (error) {
-    console.error("\x1b[31m", "Error writing the file:", error);
-  }
-}
-
-writeNewHtmlFile();
+cahe.writeNewHtmlFile();
